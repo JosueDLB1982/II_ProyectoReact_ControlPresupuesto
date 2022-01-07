ed428bbe17621f1223111e6f52262e6871ed8dff
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-07 19:38:43 -0300

    Añadido boton para abrir componente de ingresar nuevo gasto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0)
@@ -13,6 +14,17 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+
+      {isValidPresupuesto && ( /* No necesito validar mas de una condición, con el && verifico si se cumple para mostar el ícono de añadir gast */
+        <div className='nuevo-gasto'> {/* Con eso es suficiente */}
+        <img 
+          src={IconoNuevoGasto}
+          alt='Icono Nuevo Gasto'
+        />
+      </div>
+      )}
+
+      
     </div>
   )
 }
