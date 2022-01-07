709b89fcef94ddf5961f1b0bec4a0bb7f8a0d268
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-07 15:33:39 -0300

    Validando que el presupuesto sea valido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto'
 
-const Header = () => {
+const Header = ({ presupuesto, setPresupuesto }) => {
     return (
-       <header>
-           <h1>Planificador de Gastos</h1>
+        <header>
+            <h1>Planificador de Gastos</h1>
 
-           <NuevoPresupuesto/>
-       </header>
+            <NuevoPresupuesto
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+            />
+        </header>
     )
 }
 
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import {useState} from 'react'
+import Mensaje from './Mensaje'
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+    const [mensaje, setMensaje] = useState('    ')
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault()
+        if(!Number(presupuesto) || Number(presupuesto) < 0){
+            setMensaje('No es un presupuesto válido')
+        } else {
+            setMensaje('Es un presupuesto válido')
+        }
+    }
+
     return (
         <div className='contenedor-presupuesto contenedor sombra'>
-            <form className='formulario'>
+            <form onSubmit={handlePresupuesto} className='formulario'>
                 <div className='campo'>
                     <label>Definir Presupuesto</label>
                     <input
                         className='nuevo-presupuesto'
                         type='text'
                         placeholder='Añade tu Presupuesto'
+                        value={presupuesto}
+                        onChange={e => setPresupuesto(e.target.value)}
                     />
                 </div>
                     <input type="submit" value='Añadir' />
+                    {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
             </form>
         </div>
     )
