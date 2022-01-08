7fb5e8b753e5a25b0db5b079fca31045367ff789
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-08 13:22:23 -0300

    Creando state para los diferentes campos del formulario

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react'
 import CerrarVtn from '../img/cerrar.svg'
 
 const Modal = ({ setModal, animarModal, setAnimarModal }) => {
+    const [nombre, setNombre] = useState('')
+    const [cantidad, setCantidad] = useState(0)
+    const [categoria, setCategoria] = useState('')
+
     const ocultarModal = () => { /* Se agrega este setTimeout, para que al cierre se vea transición */
         setTimeout(() => {
             setModal(false)
@@ -25,6 +30,8 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
                         id='nombre'
                         type="text"
                         placeholder='Añade el Nombre del Gasto'
+                        value={nombre}
+                        onChange={e => setNombre(e.target.value)} /* Se le asignará al state el valor que el usuario coloque en el input */
                     />
                 </div>
 
@@ -32,22 +39,28 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
                     <label htmlFor="cantidad">Cantidad</label>
                     <input
                         id='cantidad'
-                        type="text"
+                        type="number"
                         placeholder='Añade la Cantidad de Presupuesto Asignado al Gasto'
+                        value={cantidad}
+                        onChange={e => setCantidad(Number(e.target.value))}
                     />
                 </div>
 
                 <div className='campo'>
                     <label htmlFor="categoria">Categoría del Gasto</label>
-                    <select id="categoria">
-                        <option value="">-- Seleccione --</option>
-                        <option value="ahorro">Ahorro</option>
-                        <option value="comida">Comida</option>
-                        <option value="casa">Casa</option>
-                        <option value="gastos">Gastos</option>
-                        <option value="ocio">Ocio</option>
-                        <option value="salud">Salud</option>
-                        <option value="suscripciones">Suscripciones</option>
+                    <select
+                        id="categoria"
+                        value={categoria}
+                        onChange={e => setCategoria(e.target.value)}
+                    >
+                            <option value="">-- Seleccione --</option>
+                            <option value="ahorro">Ahorro</option>
+                            <option value="comida">Comida</option>
+                            <option value="casa">Casa</option>
+                            <option value="gastos">Gastos</option>
+                            <option value="ocio">Ocio</option>
+                            <option value="salud">Salud</option>
+                            <option value="suscripciones">Suscripciones</option>
                     </select>
                 </div>
 
