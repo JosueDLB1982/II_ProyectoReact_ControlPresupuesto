c4216f7f9fba380531394ad68b82550fae4157c1
Josue_Lopez <josuedlopez1982@gmail.com>
2022-01-10 11:48:46 -0300

    Añadiendo efecto Swipe (deslizar) para editar o eliminar gastos

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,3 +1,12 @@
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions,
+} from 'react-swipeable-list'
+import 'react-swipeable-list/dist/styles.css'
+
 import { formatearFecha } from '../helpers'
 import IconoAhorro from '../img/icono_ahorro.svg'
 import IconoCasa from '../img/icono_casa.svg'
@@ -19,24 +28,34 @@ const diccionarioIconos = {
 
 const Gasto = ({ gasto }) => {
     const { nombre, cantidad, categoria, id, fecha } = gasto
+
+    const leadingActions = () => console.log('Editar')
+    const trailingActions = () => console.log('Eliminar')
     return (
-        <div className="gasto sombra">
-            <div className="contenido-gasto">
-                <img
-                    src={diccionarioIconos[categoria]}
-                    alt="Icono Gasto"
-                />
-                <div className="descripcion-gasto">
-                    <p className="categoria">{categoria === "varios" ? "Gastos Varios" : categoria}</p> {/* Cuando la categoría sea varios, muestra gastos varios, de resto, muestra categoría */}
-                    <p className="nombre-gasto">{nombre}</p>
-                    <p className="fecha-gasto">
-                        Agregado el: {' '}
-                        <span>{formatearFecha(fecha)}</span>
-                    </p>
+        <SwipeableList>
+            <SwipeableListItem
+                leadingActions={leadingActions}
+                trailingActions={trailingActions}
+            >
+                <div className="gasto sombra">
+                    <div className="contenido-gasto">
+                        <img
+                            src={diccionarioIconos[categoria]}
+                            alt="Icono Gasto"
+                        />
+                        <div className="descripcion-gasto">
+                            <p className="categoria">{categoria === "varios" ? "Gastos Varios" : categoria}</p> {/* Cuando la categoría sea varios, muestra gastos varios, de resto, muestra categoría */}
+                            <p className="nombre-gasto">{nombre}</p>
+                            <p className="fecha-gasto">
+                                Agregado el: {' '}
+                                <span>{formatearFecha(fecha)}</span>
+                            </p>
+                        </div>
+                    </div>
+                    <p className='cantidad-gasto'>${cantidad}</p>
                 </div>
-            </div>
-            <p className='cantidad-gasto'>${cantidad}</p>
-        </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
 
